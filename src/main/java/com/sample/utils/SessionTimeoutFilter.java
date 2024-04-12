343b93938601f1f6bb5e8bd2e2728c0379712121
cfv21 <cfv21@fsu.edu>
2024-04-12 16:07:18 -0400

    Initial commit

---
+++ src/main/java/com/sample/utils/SessionTimeoutFilter.java
@@ -0,0 +1,45 @@
+package com.sample.utils;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.IOException;
+import java.time.Instant;
+
+public class SessionTimeoutFilter implements Filter {
+    private static final long TIMEOUT_DURATION = 10 * 60 * 1000; // 10 minutes in milliseconds
+    private static final String LOGIN_TIME_ATTRIBUTE = "loginTime";
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        // Initialization code, if any
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+        HttpSession session = httpRequest.getSession(false);
+        if (session != null) {
+            Long loginTime = (Long) session.getAttribute(LOGIN_TIME_ATTRIBUTE);
+            if (loginTime != null && hasSessionTimedOut(loginTime)) {
+                // Session has timed out, invalidate the session
+                session.invalidate();
+                // Redirect or return an error response as appropriate
+                httpResponse.sendRedirect(httpRequest.getContextPath() + "/login.jsp");
+                return;
+            }
+        }
+
+        chain.doFilter(request, response);
+    }
+
+    private boolean hasSessionTimedOut(long loginTime) {
+        long currentTime = Instant.now().toEpochMilli();
+        return currentTime - loginTime > TIMEOUT_DURATION;
+    }
+
+    @Override
+    public void destroy() {
+        // Cleanup code, if any
+    }
+}
